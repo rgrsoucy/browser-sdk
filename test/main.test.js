f57bfb6a72669e0316ac4cfa5c69e8a2562eb23a
Katie McGinley <katie@mcgnly.com>
2016-06-01 14:22:44 +0200

    add blank tests for device class

--- test/main.test.js
+++ test/main.test.js
@@ -2,9 +2,81 @@ import expect from 'expect';
 import Relayr from '../src/main';
 
 describe('Main', () => {
-  describe('can be instantiated', () => {
-    it('should be a constructor', () => {
-    	expect(new Relayr).toBeA(Relayr);
-    });
-  });
-});
+        describe('can be instantiated', () => {
+            it('should be a constructor', () => {
+                expect(new Relayr).toBeA(Relayr);
+            });
+            //check that relayr should be instantiated with tools, ID, etc (??)
+        });
+
+
+
+
+        describe('Device', () => {
+            let device;
+            beforeEach(() => {
+                //stub a device object returned from the cloud
+            })
+
+            describe('can be instantiated', () => {
+                it('should be a constructor', () => {
+                    expect(new Device).toBeA(Device);
+                });
+            });
+
+            // getDevice
+            describe('#getDevice', () => {
+                it('should get the device', () => {
+
+                });
+            });
+
+            // getDeviceData
+            describe('#getDeviceData', () => {
+                it('should get the device data', () => {
+
+                });
+            });
+
+            // getDeviceState
+            describe('#getDeviceState', () => {
+                it('should get the device state', () => {
+
+                });
+            });
+
+            // sendCommand
+            describe('#sendCommand', () => {
+                it('should send a command with good id info', () => {
+
+                });
+
+                it('should send a command with bad id info', () => {
+
+                });
+            });
+
+        });
+
+
+
+
+
+        //Group
+        // getAllGroups
+
+        //Model
+        // getAllModels
+
+        //Transmitter
+        // getTransmitters
+        // deleteTransmitter
+        // updateTransmitter
+
+        //User
+        //getToken
+        //setToken
+        //hasToken
+        //getUserInfo
+        //logout
+        // getAllDevices
