a4bde3bf231a528596131f4b46cf711c1a97c7b2
Jon Tore Hafstad <jon@relayr.io>
2016-06-03 17:42:39 +0200

    A simple OAuth class with login functionality and parsing of URL with access token

--- authorization/oauth2.js
+++ authorization/oauth2.js
@@ -1,16 +1,41 @@
 class Oauth2 {
-  constructor(project) {
-    this.project = project;
-  }
+    constructor(options) {
+        this.appId = options.appId;
+        this.redirectURI = options.redirectURI;
+    }
 
-  get token() {
-    return this.token();
-  }
+    login(optUser, ctx) {
+        if (!this.redirectURI) {
+            throw Error('OAuth2 a valid redirect uri must be provided on login');
+        } else if (!this.appId) {
+            throw Error('OAuth2 a valid app ID must be provided on login');
+        }
 
-  login(optUser, ctx) {
-    //We redirect to relayr Login and start the login process
+        this._loginRedirect(`https://api.relayr.io/oauth2/auth?client_id=${this.appId}&redirect_uri=${this.redirectURI}&response_type=token&scope=access-own-user-info+configure-devices`);
+    }
 
-  }
+    _loginRedirect() {}
+
+    parseToken(tokenURL) {
+        var parts = tokenURL.split('#');
+        if (parts[0].length === 0 || parts[1].length === 0) {
+            throw Error('The provided URL is not correctly formatted');
+        }
+        var queryParams = parts[1].split('&');
+        var authParams = queryParams.reduce(function(accumulator, pair) {
+            var tuple = pair.split('=');
+            accumulator[tuple[0]] = tuple[1];
+            return accumulator;
+        }, {});
+
+        if (!authParams.token_type) {
+            throw Error('The provided URL does not contain a access token');
+        }
+
+        this.token = authParams.token_type + ' ' + authParams.access_token;
+    }
+
+
 }
 
 module.exports =  Oauth2;
