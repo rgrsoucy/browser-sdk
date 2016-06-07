631a2d01de598bd1dd1bfb01b3ebafcef206c299
Katie McGinley <katie@mcgnly.com>
2016-06-07 16:08:25 +0200

    start getDeviceData, start tests for it

--- entities/Device.js
+++ entities/Device.js
@@ -1,30 +1,37 @@
+import ajax from '../tools/ajax.js';
+
 export
 default class Device {
-    constructor(options) {
-        this.deviceId = options.deviceId;
-        this.userId = options.userId;
-    }
+  constructor(options) {
+    this.deviceId = options.deviceId;
+    this.userId = options.userId;
+  }
 
-    getDeviceData() {
-        if (!(this.deviceId)) {
-            throw new Error('Provide the deviceId during instantiation');
-        }
-        return new Promise(function(resolve, reject) {
-            let callConfig = {
-                url: 'devices/' + this.deviceId,
-                type: 'GET'
-            }
-            _ajax(callConfig, function(deviceInstance) {
-                resolve(deviceInstance);
-            }, function(error) {
-                reject(error);
-            });
-        });
+  getDeviceData() {
+    if (!(this.deviceId)) {
+      throw new Error('Provide the deviceId during instantiation');
     }
+    return new Promise(function(resolve, reject) {
+      let callConfig = {
+        url: 'devices/' + this.deviceId,
+        type: 'GET'
+      }
 
-    _ajax(callConfig) {
+      //resolve does this:
+      function fulfilled() {
+        ajax(callConfig).then(
+          //get the resolved promise back from ajax with json response text
+          function() {
+            resolve(device.incomingData)
+          }).catch(error); //if the ajax doesn't resolve right
+      },
+      //reject does this;
+      function rejected() {
+        reject(error);
+      }
+    });
+  }
 
-    }
 };
 
 // getDeviceState
--- test/device.test.js
+++ test/device.test.js
@@ -3,39 +3,49 @@ import Device from '../entities/Device.js';
 import chai from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
+
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+
+
 var expect = chai.expect;
 chai.use(sinonChai);
 
 let deviceInstance;
 
 describe('Device', function() {
+  beforeEach(function() {
+    let options = {
+      deviceId: 'fakeDeviceId',
+      userId: 'fakeUserId'
+    };
+
+    deviceInstance = new Device(options);
+  });
+
+  describe('#getDeviceData', function() {
     beforeEach(function() {
-        let options = {
-            deviceId: 'fakeDeviceId',
-            userId: 'fakeUserId'
-        };
-
-        deviceInstance = new Device(options);
+      sinon.spy(deviceInstance, '_ajax');
     });
 
-    describe('#getDeviceData', function() {
-        beforeEach(function() {
-            sinon.spy(deviceInstance, '_ajax');
-        });
-
-        it('should throw an error if no deviceId given to look up', function() {
-            deviceInstance.deviceId = null;
-            var fn = function() {
-                deviceInstance.getDevice();
-            };
-            expect(fn).to.throw(Error);
-        });
-
-        it('should call _ajax with the right structure', function() {
-            deviceInstance.getDeviceData();
-            expect(deviceInstance._ajax).to.have.been.calledWith(sinon.match('devices/fakeDeviceId'), sinon.match('GET'));
-        });
+    it('should throw an error if no deviceId given to look up', function() {
+      deviceInstance.deviceId = null;
+      var fn = function() {
+        deviceInstance.getDevice();
+      };
+      expect(fn).to.throw(Error);
     });
+
+    it('should do something with promises', function() {
+      var blah = 'foo';
+
+      var result = deviceInstance.getDeviceData();
+
+      expect(result).to.eventually.equal(blah);
+    });
+
+
+  });
 });
 
 //  getDevice
