b68c78a1fa92b214430137231e5ffe41f2a566bb
Katie McGinley <katie@mcgnly.com>
2016-06-07 11:24:08 +0200

    set up Device class first crack at getDeviceData

--- entities/Device.js
+++ entities/Device.js
@@ -1,5 +1,32 @@
-// getDeviceData
-// getDevice
+export
+default class Device {
+    constructor(deviceId, userId) {
+        this.deviceId = deviceId;
+        this.userId = userId;
+    }
+
+    getDeviceData(deviceId) {
+        if (!(this.deviceId)) {
+            throw new Error('Provide the deviceId during instantiation');
+        }
+        return new Promise(function(resolve, reject) {
+            let callConfig = {
+                url: 'devices/' + this.deviceId,
+                type: 'GET'
+            }
+            _ajax(callConfig, function(deviceInstance) {
+                resolve(deviceInstance);
+            }, function(error) {
+                reject(error);
+            });
+        });
+    }
+
+    _ajax(callConfig) {
+
+    }
+};
+
 // getDeviceState
 // getAllDevices
 // sendCommand
