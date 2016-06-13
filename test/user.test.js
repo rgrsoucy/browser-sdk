cddde9630f5a3563943870e8a05e7a8d49778dc3
Katie McGinley <katie@mcgnly.com>
2016-06-13 14:41:45 +0200

    test getMyTransmitters

--- test/user.test.js
+++ test/user.test.js
@@ -55,8 +55,31 @@ describe('User', function() {
         'Content-Type': 'text/json'
       }, JSON.stringify(userStub));
     });
+  });
+
+  describe('#getMyTransmitters', function() {
+
+    it('should resolve a promise with transmitter info', function(done) {
+      let transmitterStub = [{
+        "id": "c30f9ca2-db38-496b-a833-ca67cb4dc3c8",
+        "secret": "AOf3Ft47lvDjgKX",
+        "owner": "c70faa9f-5eda-49d8-be91-a7e4b1beeca1",
+        "name": "Test transmitter",
+        "topic": "/v1/396e822e-9d04-40c1-a049-8b95d3fd7e36/",
+        "integrationType": "wunderbar2"
+      }]
 
 
+      userInstance.ajax.customXHR = this.xhr;
+      userInstance.getMyTransmitters().then((result) => {
+        expect(result).to.deep.equal(transmitterStub[0]);
+        done();
+      });
 
+
+      this.requests[0].respond(200, {
+        'Content-Type': 'text/json'
+      }, JSON.stringify(groupStub));
+    });
   });
 });
