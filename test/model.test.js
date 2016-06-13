0b94b3f13e6f5b42bbcb7b11e9ca973e474ffa9c
Katie McGinley <katie@mcgnly.com>
2016-06-13 11:07:10 +0200

    change sampleModel into array, and check that it is included in the response

--- test/model.test.js
+++ test/model.test.js
@@ -22,6 +22,7 @@ describe('Model', function() {
 
     modelInstance = new Model(fakeConfig);
 
+
     this.xhr = sinon.useFakeXMLHttpRequest();
     // console.log(this.xhr);
     this.requests = [];
@@ -31,53 +32,61 @@ describe('Model', function() {
     }.bind(this);
 
   });
-});
 
-describe('#getAllModels', function() {
-  it('should return an object of models available on the platform', function() {
 
-    let sampleModel = {
-      "id": "ebd828dd-250c-4baf-807d-69d85bed065b",
-      "name": "Wunderbar Bridge Module",
-      "manufacturer": "Relayr GmbH",
-      "readings": [{
-        "meaning": "raw",
-        "unit": "raw"
-      }],
-      "commands": [{
-        "command": "cmd",
-        "path": "led",
-        "unit": "integer",
-        "maximum": 1,
-        "minimum": 0
-      }],
-      "firmwareVersions": [{
-        "version": "1.0.0",
-        "configuration": {
-          "schema": {
-            "title": "Relayr configuration schema",
-            "type": "object",
-            "properties": {
-              "frequency": {
-                "description": "Frequency of the sensor updates in milliseconds",
-                "type": "integer",
-                "minimum": 200
-              }
+  describe('#getAllModels', function() {
+    it('should return an object of models available on the platform', function(done) {
+
+      let sampleModel = [{
+        "id": "ebd828dd-250c-4baf-807d-69d85bed065b",
+        "name": "Wunderbar Bridge Module",
+        "manufacturer": "Relayr GmbH",
+        "readings": [{
+          "meaning": "raw",
+          "unit": "raw"
+        }],
+        "commands": [{
+          "command": "cmd",
+          "path": "led",
+          "unit": "integer",
+          "maximum": 1,
+          "minimum": 0
+        }],
+        "firmwareVersions": [{
+          "version": "1.0.0",
+          "configuration": {
+            "schema": {
+              "title": "Relayr configuration schema",
+              "type": "object",
+              "properties": {
+                "frequency": {
+                  "description": "Frequency of the sensor updates in milliseconds",
+                  "type": "integer",
+                  "minimum": 200
+                }
+              },
+              "required": [
+                "frequency"
+              ]
             },
-            "required": [
-              "frequency"
-            ]
-          },
-          "defaultValues": {
-            "frequency": 1000
+            "defaultValues": {
+              "frequency": 1000
+            }
           }
-        }
+        }]
       }]
-    }
-    modelInstance.getAllModels().then((result) => {
-      expect(result).to.include(sampleModel);
-      done();
-    });
+      modelInstance.ajax.customXHR = this.xhr;
+      modelInstance.getAllModels().then((result) => {
+        console.log(result);
+        expect(result).to.include(sampleModel[0]);
+        done();
+      });
 
+      this.requests[0].respond(200, {
+        'Content-Type': 'text/json'
+      }, JSON.stringify(sampleModel));
+
+
+    });
   });
 });
