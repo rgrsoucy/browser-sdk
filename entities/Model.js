62d746740a2bcdb72f1ce54d82588fee1df17610
Katie McGinley <katie@mcgnly.com>
2016-06-13 10:37:25 +0200

    implemented getAllModels and test

--- entities/Model.js
+++ entities/Model.js
@@ -1,1 +1,22 @@
 // getAllModels
+import Ajax from '../tools/ajax.js'
+
+export
+default class Model {
+  constructor(config) {
+    this.config = config;
+    this.ajax = new Ajax(config.ajax);
+  }
+
+
+  getAllModels() {
+    return new Promise((resolve, reject) => {
+      this.ajax.get('device-models?limit=100000', ).then((response) => {
+        this.allModels = response;
+        resolve(response)
+      }).catch((error) => {
+        reject(error);
+      });
+    });
+  }
+}
