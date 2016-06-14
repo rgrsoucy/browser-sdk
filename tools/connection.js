bad7b4190f9fb88e13664fcebb003bad7538801d
Smbat Yeranyan <smbat.yeranyan@gmail.com>
2016-06-14 10:57:58 +0200

    abstracted flush and removed timeout as it made no sense anymore

--- tools/connection.js
+++ tools/connection.js
@@ -9,14 +9,23 @@ function connection() {
             self._dataSubscriber(data)
         } else {
             self.buffer.push(data);
-            self.retry();
         }
+    }
+
+    this._flush = () => {
+        if (self._dataSubscriber) {
+            for (var i = self.buffer.length - 1; i >= 0; i--) {
+                self._dataSubscriber(self.buffer[i]) && self.buffer.splice(i, 1);
+            }
+        }
+        return;
     }
 
     this.on = function(event, _dataSubscriber) {
         switch (event) {
             case "data":
                 self._dataSubscriber = _dataSubscriber;
+                this._flush();
                 break;
             case "error":
                 break;
@@ -27,12 +36,4 @@ function connection() {
         }
     }
 
-    this.retry = () => {
-        setTimeout(() => {
-            for (var i = self.buffer.length - 1; i >= 0; i--) {
-                if (self._dataSubscriber) self._dataSubscriber(self.buffer[i]) && self.buffer.splice(i, 1);
-            }
-        }, 1000)
-        return;
-    }
 }
