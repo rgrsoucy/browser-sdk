9b95077cdd770d80e0bd3fe7241e17072682fe4a
Katie McGinley <katie@mcgnly.com>
2016-07-01 11:21:37 +0200

    change transmitterId to id in test as well

--- test/transmitter.test.js
+++ test/transmitter.test.js
@@ -12,112 +12,110 @@ let fakeConfig;
 
 
 describe('Transmitter', function() {
-  beforeEach(function() {
-    fakeConfig = {
-      transmitterId: 'fakeTransmitterId',
-      secret: 'fakeSecret',
-      name: 'fakeTransmitterName',
-      topic: 'fakeTopic',
-      owner: 'fakeOwner',
-      integrationType: 'fakeIntegrationType',
-      ajax: {
-        url: 'fakeURL',
-        token: '12345',
-        tokenType: 'Bearer'
-      }
-    };
+    beforeEach(function() {
+        fakeConfig = {
+            id: 'fakeID',
+            secret: 'fakeSecret',
+            name: 'fakeTransmitterName',
+            topic: 'fakeTopic',
+            owner: 'fakeOwner',
+            integrationType: 'fakeIntegrationType',
+            ajax: {
+                url: 'fakeURL',
+                token: '12345',
+                tokenType: 'Bearer'
+            }
+        };
 
-    transmitterInstance = new Transmitter(fakeConfig);
+        transmitterInstance = new Transmitter(fakeConfig);
 
-    this.xhr = sinon.useFakeXMLHttpRequest();
-    this.requests = [];
+        this.xhr = sinon.useFakeXMLHttpRequest();
+        this.requests = [];
 
-    this.xhr.onCreate = function(xhr) {
-      this.requests.push(xhr);
-    }.bind(this);
-  });
-
-  describe('#updateTransmitter', function() {
-
-    it('should throw an error if no transmitterId given to look up', function() {
-      transmitterInstance.transmitterId = null;
-      var fn = function() {
-        transmitterInstance.updateTransmitter();
-      };
-      expect(fn).to.throw(Error);
+        this.xhr.onCreate = function(xhr) {
+            this.requests.push(xhr);
+        }.bind(this);
     });
 
-    it('should give a body of parameters at all to update', function() {
-      var fn = function() {
-        transmitterInstance.updateTransmitter();
-      };
-      expect(fn).to.throw(Error);
+    describe('#updateTransmitter', function() {
+
+        it('should throw an error if no id given to look up', function() {
+            transmitterInstance.id = null;
+            var fn = function() {
+                transmitterInstance.updateTransmitter();
+            };
+            expect(fn).to.throw(Error);
+        });
+
+        it('should give a body of parameters at all to update', function() {
+            var fn = function() {
+                transmitterInstance.updateTransmitter();
+            };
+            expect(fn).to.throw(Error);
+        });
+
+        it('should have something in the body', function() {
+            let body = {};
+            var fn = function() {
+                transmitterInstance.updateTransmitter(body);
+            };
+            expect(fn).to.throw(Error);
+        });
+
+        it('should give relevant parameters to update on a transmitter in the body', function() {
+            let body = {
+                pets: 'unicorn',
+                abilities: 'flying'
+            };
+
+            var fn = function() {
+                transmitterInstance.updateTransmitter(body);
+            };
+            expect(fn).to.throw(Error);
+        });
+
+        it('should return the correct updated transmitter object based on multiple changes', function(done) {
+            let patch = {
+                owner: 'bob',
+                name: 'Im a thing'
+            };
+            let response;
+
+            transmitterInstance.updateTransmitter(patch, {}).then((response) => {
+                expect(patch).to.deep.equal(response);
+                done();
+            });
+
+            //this is the api
+            this.requests[0].respond(200, {
+                'Content-Type': 'text/json'
+            }, JSON.stringify(patch));
+        });
     });
 
-    it('should have something in the body', function() {
-      let body = {};
-      var fn = function() {
-        transmitterInstance.updateTransmitter(body);
-      };
-      expect(fn).to.throw(Error);
+
+    describe('#deleteTransmitter', function() {
+        it('should throw an error if no id given to look up', function() {
+            transmitterInstance.id = null;
+            var fn = function() {
+                transmitterInstance.deleteTransmitter();
+            };
+            expect(fn).to.throw(Error);
+        });
+
+        it('should delete the transmitterInstance', function(done) {
+            let data = {};
+
+            transmitterInstance.deleteTransmitter(data).then((response) => {
+                expect(response).to.be.defined;
+                done();
+            });
+
+            //this is the api
+            this.requests[0].respond(204, {
+                'Content-Type': 'text/json'
+            }, JSON.stringify(data));
+        });
+
     });
-
-    it('should give relevant parameters to update on a transmitter in the body', function() {
-      let body = {
-        pets: 'unicorn',
-        abilities: 'flying'
-      };
-
-      var fn = function() {
-        transmitterInstance.updateTransmitter(body);
-      };
-      expect(fn).to.throw(Error);
-    });
-
-    it('should return the correct updated transmitter object based on multiple changes', function(done) {
-      let patch = {
-        owner: 'bob',
-        name: 'Im a thing'
-      };
-      let response;
-
-
-
-      transmitterInstance.updateTransmitter(patch, true).then((response) => {
-        expect(patch).to.deep.equal(response);
-        done();
-      });
-
-      //this is the api
-      this.requests[0].respond(200, {
-        'Content-Type': 'text/json'
-      }, JSON.stringify(patch));
-    });
-  });
-
-
-  describe('#deleteTransmitter', function() {
-    it('should throw an error if no transmitterId given to look up', function() {
-      transmitterInstance.transmitterId = null;
-      var fn = function() {
-        transmitterInstance.deleteTransmitter();
-      };
-      expect(fn).to.throw(Error);
-    });
-
-    it('should delete the transmitterInstance', function(done) {
-      let data = {};
-
-      transmitterInstance.deleteTransmitter(data).then((response) => {
-        expect(response).to.be.defined;
-        done();
-      });
-
-      //this is the api
-      this.requests[0].respond(204, {
-        'Content-Type': 'text/json'
-      }, JSON.stringify(data));
-    });
-
-  });
 });
