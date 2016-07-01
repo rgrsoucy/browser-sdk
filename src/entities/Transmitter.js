b4467c8d7f56e7166e1248899dbc030c95bcd427
Katie McGinley <katie@mcgnly.com>
2016-07-01 11:10:49 +0200

    clean up transmitterId name to make more consistent with what comes back from the api, as well as what's done in Device

--- src/entities/Transmitter.js
+++ src/entities/Transmitter.js
@@ -3,7 +3,7 @@ import Ajax from '../tools/ajax.js';
 export
 default class Transmitter {
     constructor(config) {
-        this.transmitterId = config.transmitterId;
+        this.id = config.id;
         this.secret = config.secret;
         this.name = config.name;
         this.topic = config.topic;
@@ -13,11 +13,11 @@ default class Transmitter {
     }
 
     deleteTransmitter(opts) {
-        if (!(this.transmitterId)) {
-            throw new Error('Provide the transmitterId during instantiation');
+        if (!(this.id)) {
+            throw new Error('Provide the id during instantiation');
         }
         return new Promise((resolve, reject) => {
-            this.ajax.delete(`/transmitters/${this.transmitterId}`, opts)
+            this.ajax.delete(`/transmitters/${this.id}`, opts)
                 .then((response) => {
                     //right now the object hangs around, but on the cloud it is gone
                     resolve(response);
@@ -28,8 +28,8 @@ default class Transmitter {
     }
 
     updateTransmitter(patchBody, opts) {
-        if (!(this.transmitterId)) {
-            throw new Error('Provide the transmitterId during instantiation');
+        if (!(this.id)) {
+            throw new Error('Provide the id during instantiation');
         } else if (!(patchBody)) {
             throw new Error('Provide a patch of parameters to update');
         } else if (!(Object.keys(patchBody).length)) {
@@ -43,10 +43,9 @@ default class Transmitter {
         }
 
         return new Promise((resolve, reject) => {
-            console.log('================================', `${this.transmitterId}`);
-            this.ajax.patch(`/transmitters/${this.transmitterId}`, patchBody, opts)
+            this.ajax.patch(`/transmitters/${this.id}`, patchBody, opts)
                 .then((response) => {
-                    this.transmitterId = response.transmitterId,
+                    this.id = response.id,
                     this.secret = response.secret,
                     this.name = response.name,
                     this.topic = response.topic,
