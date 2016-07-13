6a23569ca1122110dfecfce8c34aa20ec2d8cb68
relayrrobot <server-team@relayr.de>
2016-07-13 15:52:00 +0000

    Jenkins dist build

--- dist/relayr-browser-sdk.js
+++ dist/relayr-browser-sdk.js
@@ -1,4 +1,4 @@
-//Latest build: 07-13-16 15:45
+//Latest build: 07-13-16 15:51
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
